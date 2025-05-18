2202b310c5e8b6016f4e051c2764d19999d084e4
Darwishy <rtoet18@hotmail.com>
2025-05-18 19:57:36 +0200

    safsa

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    num_words = word_count(text)
     print(text)
+    print(f"{num_words} words found in the document")
+ 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+
+
+
+def word_count(text):
+    num_words = len(text.split())
+    return num_words
 
 main()
