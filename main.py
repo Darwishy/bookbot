bf69fefca93158ca56e5c9aeee4b8f5b767b61f7
Darwishy <rtoet18@hotmail.com>
2025-05-18 20:27:29 +0200

    sdfdfs

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = word_count(text)
-    print(text)
+    num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
- 
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-
-
-
-def word_count(text):
-    num_words = len(text.split())
-    return num_words
 
 main()
